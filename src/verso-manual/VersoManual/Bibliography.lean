658be9c519f567bfaf025339c7c1e3aef5eec770
Alok Singh <alokbeniwal@gmail.com>
2025-05-22 01:21:56 -0400

    chore(manual): inline arrayOrd shim and drop external compat import

--- src/verso-manual/VersoManual/Bibliography.lean
+++ src/verso-manual/VersoManual/Bibliography.lean
@@ -11,7 +11,6 @@ import Verso.Output.Html
 import Verso.Output.TeX
 import VersoManual.Basic
 import VersoManual.Marginalia
-import VersoManual.Compat
 
 
 open Lean Elab
@@ -62,6 +61,7 @@ structure ArXiv where
 deriving ToJson, FromJson, BEq, Hashable
 
 section
+def _root_.Ord.arrayOrd {α} [Ord α] : Ord (Array α) := inferInstance
 attribute [local instance] Ord.arrayOrd
 deriving instance Ord for InProceedings, ArXiv
 end
@@ -147,12 +147,12 @@ def Citable.bibHtml (go : Doc.Inline Genre.Manual → HtmlT Manual (ReaderT Exte
   match c with
   |  .inProceedings p =>
     let authors ← andList <$> p.authors.mapM go
-    return {{ {{authors}} s!", {p.year}. " {{ link {{"“" {{← go p.title}} "”"}} }} ". In " <em>{{← go p.booktitle}}"."</em>{{(← p.series.mapM go).map ({{" (" {{·}} ")" }}) |>.getD .empty}} }}
+    return {{ {{authors}} s!", {p.year}. " {{ link {{""" {{← go p.title}} ""}} }} ". In " <em>{{← go p.booktitle}}"."</em>{{(← p.series.mapM go).map ({{" (" {{·}} ")" }}) |>.getD .empty}} }}
   | .thesis p =>
     return {{ {{← go p.author}} s!", {p.year}. " <em>{{link (← go p.title)}}</em> ". " {{← go p.degree}} ", " {{← go p.university}} }}
   | .arXiv p =>
     let authors ← andList <$> p.authors.mapM go
-    return {{ {{authors}} s!", {p.year}. " {{ link {{"“" {{← go p.title}} "”"}} }} ". arXiv:" {{p.id}} }}
+    return {{ {{authors}} s!", {p.year}. " {{ link {{"" {{← go p.title}} ""}} }} ". arXiv:" {{p.id}} }}
 where
   wrap (content : Html) : Html := {{<span class="citation">{{content}}</span>}}
   link (title : Html) : Html :=
@@ -169,12 +169,12 @@ def Citable.inlineHtml
   | .textual =>
     let out : Array Html ← ps.toArray.mapM fun p => do
       let m ← p.bibHtml go
-      pure <| {{ {{← authorHtml p}} s!" ({p.year})"}} ++ Marginalia.html m
+      pure <| {{ {{← authorHtml p}} s!" ({p.year})"}} ++ Marginalia.html m
     pure <| andList out
   | .parenthetical =>
     let out : Array Html ← ps.toArray.mapM fun p => do
       let m ← p.bibHtml go
-      pure <| {{" (" {{← authorHtml p}} s!", {p.year})"}} ++ Marginalia.html m
+      pure <| {{" ({← authorHtml p}} s!", ({p.year})"}} ++ Marginalia.html m
     pure <| andList out
   | .here => do
     pure <| andList (← ps.toArray.mapM (·.bibHtml go))
